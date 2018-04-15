4636c24f679b1b398bfbf2aa1cc9c6b26b6cfc00
Cruyun <1045671796@qq.com>
2018-04-15 16:11:51 +0800

    init project

---
+++ build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/cocoa_init.m" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/cocoa_joystick.m" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/cocoa_monitor.m" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/cocoa_time.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/cocoa_window.m" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/context.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/init.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/input.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/monitor.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/nsgl_context.m" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/posix_tls.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/vulkan.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/cairuyun/Desktop/Projects/DrawSVG/CMU462/deps/glfw/src/window.c" "/Users/cairuyun/Desktop/Projects/DrawSVG/build/CMU462/deps/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CMU462/deps/glfw/include"
+  "../CMU462/deps/glfw/src"
+  "CMU462/deps/glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
